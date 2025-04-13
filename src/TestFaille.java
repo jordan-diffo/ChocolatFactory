5634a5ea69fc993a88a98c63edfac453a30fe285
Jordan Diffo <jordan.ndyj@gmail.com>
2025-04-13 13:51:00 -0400

    Ajout de code pour tester l'analyse CodeQL

---
+++ src/TestFaille.java
@@ -0,0 +1,6 @@
+public class TestFaille {
+    public static void main(String[] args) {
+        String secret = "monMotDePasse123";
+        System.out.println("Le mot de passe est : " + secret);
+    }
+}
